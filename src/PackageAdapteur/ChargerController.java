1ce985ce7b9bb4384d38d41e9aaedd1f65c2e9ee
Guillaume boutin <boutinguillaume@hotmail.fr>
2017-06-15 14:44:31 +0200

    Exercice Adapter v2

--- src/PackageAdapteur/ChargerController.java
+++ src/PackageAdapteur/ChargerController.java
@@ -10,11 +10,14 @@ public class ChargerController {
      * @param args
      */
     public static void main(String[] args){
+        Chargeur c = new Chargeur();
 
         IChargeable telSam = new AdapterSamsoule(new PortableSamSaoule());
+        c.brancherPortable(telSam);
         telSam.Recharger(10);
 
         IChargeable telSonny = new AdapterSonneEricSonne(new PortableSonneEricSonne());
+        c.brancherPortable(telSonny);
         telSam.Recharger(5);
     }
 }
